275f8e32ac80e65213b8efeb25282c9cfad02511
Adailton Saraiva <s.adailton@gmail.com>
2020-06-08 23:20:12 -0300

    alteração baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http://localhost:3002/api"
+    baseURL: "https://fulllstackbackend.herokuapp.com/api"
 })
 
-export default api
+export default api;
